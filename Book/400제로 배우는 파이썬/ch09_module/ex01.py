ebe67d5560155043306cf4a44f26dfe6ebd94367
jaeyeongs <skek1511@gmail.com>
2023-04-01 17:38:14 +0900

    module

---
+++ Book/400제로 배우는 파이썬/ch09_module/ex01.py
@@ -0,0 +1,12 @@
+# 모듈로 사용할 파이썬 파일 작성
+
+def say_hello(name):
+    print("hello", name)
+
+def say_hello2(name):
+    print("good morning", name)
+
+
+if __name__ == '__main__':
+    say_hello("kim")
+    say_hello2("hong")
